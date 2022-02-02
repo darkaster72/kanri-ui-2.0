77eb7dd60151f3e92ca104131a327afaf4f4f7cd
RaviHaldwani <ravihaldwani143@gmail.com>
2022-02-02 21:21:16 +0530

    left changes

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './layout.component';
+import { LoginComponent } from './login.component';
+import { RegisterComponent } from './register.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        ReactiveFormsModule
+      
+    ],
+    declarations: [
+        LayoutComponent,
+        LoginComponent,
+        RegisterComponent
+    ]
+})
+export class AuthModule { }
